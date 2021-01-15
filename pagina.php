9f183b15519124b4888ba2721d27999d3b820254
joaqui <joaquinroman2006@gmail.com>
2021-01-14 23:02:07 -0300

    cambio git

--- pagina.php
+++ pagina.php
@@ -7,7 +7,7 @@
 	<link rel="stylesheet" type="text/css" href="estilos.css">
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Clasificados</title>
+	<title>Prueba Pagina</title>
 </head>
 <body>
 	<div class= "div1">
